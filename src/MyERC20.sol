81198888397a83525e0487a629a3121d1b61f3ec
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-04-07 12:04:09 -0400

    cmt token finished and tested

--- src/MyERC20.sol
+++ src/MyERC20.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/token/ERC20/ERC20.sol";
+import "forge-std/console.sol";
 
 
 contract MyERC20 is ERC20 {
+
+    address public owner;
 
     constructor(
         uint256 amount,
@@ -12,6 +15,35 @@ contract MyERC20 is ERC20 {
     ) 
     ERC20("cmonte", "cmt"){
         _mint(beneficiary, amount);
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function buy(uint256 cmtAmount) public payable {
+        //1 cmt is .001 eth
+        uint256 cmtPrice = 1000000000000000 * cmtAmount;
+        console.log(cmtAmount);
+        console.log(cmtPrice);
+        console.log(msg.value);
+
+        require(msg.value > 0, "Did not receive ether");
+        require(msg.value == cmtPrice,
+        "Received different amount of ETH than required, Remember 1 CMT is .001 ETH");
+        _mint(msg.sender, cmtAmount);
+    }
+
+    function burn(uint256 amount) public onlyOwner {
+        _burn(msg.sender, amount);
+    }
+
+    function withdrawal() public onlyOwner returns(bool) {
+        console.log(address(this).balance);
+        require(transfer(msg.sender, 5), "could not withdrawal");
+        return true;
     }
 
 }
--- test/MyERC20.t.sol
+++ test/MyERC20.t.sol
@@ -9,18 +9,78 @@ contract MyERC20Test is Test {
     MyERC20 public myERC20;
 
     function setUp() public {
+        myERC20 = new MyERC20(0, address(this));
+        vm.deal(address(this), 2 ether);
     }
     
-    function testDeployment(uint256 amount) public {
+    function test_Deployment(uint256 amount) public {
         myERC20 = new MyERC20(amount, address(this));
         uint256 mintBalance = myERC20.balanceOf(address(this));
         assertEq(mintBalance, amount);
     }
 
+    function test_Buy() public {
+        
+        myERC20.buy{value: 0.001 ether}(1);
+        myERC20.buy{value: 0.005 ether}(5);
+        myERC20.buy{value: 1.003 ether}(1003);
+        assertEq(myERC20.balanceOf(address(this)), 1009);
+    }
+
+    function testFail_Buy() public {
+        myERC20.buy{value: 0.002 ether}(1);
+    }
+
+    /*
+    function testFuzz_Buy(uint256 amount) public {
+        uint256 etherAmount = amount / 1000;
+        myERC20.buy{value: etherAmount ether}(amount);
+    }
+    */
+
+    function test_Burn() public {
+        myERC20.buy{value: 0.001 ether}(1);
+        myERC20.burn(1);
+        assertEq(myERC20.balanceOf(address(this)), 0);
+    }
+
+    function testFail_Burn() public {
+        myERC20.buy{value: 0.001 ether}(1);
+        myERC20.burn(2);
+    }
+
+    function test_Withdrawal() public {
+        myERC20.buy{value: 0.005 ether}(5);
+        assertTrue(myERC20.withdrawal());
+        //uint256 balance = address(new MyERC20()).balance;
+        //assertEq(balance, 1.996);
+    }
+
+
+
+
+
+    ///
+    /// tests below are unrelated, understanding only
+    ///
+    
     function testFail_Fuzzing(uint256 amount) public {
         uint256 plus2 = amount + 2;
         //assertion below should fail
         assertEq(plus2, amount);
+    }
+
+    function test_ExpectRevertDivide0() public {
+        uint256 zero = 0;
+        uint256 divide = 0;
+        vm.expectRevert();
+        divide = 32 / zero;
+    }
+
+    function test_CannotSubtract43() public {
+    uint256 testNumber = 42;
+    vm.expectRevert();
+    testNumber -= 43;
     }
 
 }
